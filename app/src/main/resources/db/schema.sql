763161af529aa6da9e7175b3f9b2af6b4b09b640
per <per@alipsa.se>
2026-04-09 20:08:56 +0200

    fixade formattering

--- app/src/main/resources/db/schema.sql
+++ app/src/main/resources/db/schema.sql
@@ -1,13 +1,5 @@
-CREATE TABLE IF NOT EXISTS schema_version
-(
-    version
-    INTEGER
-    PRIMARY
-    KEY,
-    script_name
-    VARCHAR
-(
-    255
-) NOT NULL,
+CREATE TABLE IF NOT EXISTS schema_version (
+    version INTEGER PRIMARY KEY,
+    script_name VARCHAR(255) NOT NULL,
     installed_at TIMESTAMP NOT NULL
-    );
+);
